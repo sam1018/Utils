09678286dbfffabd20d60823bc140d0b82f94aaf
Md. Samiul Hoque <samiul1018@gmail.com>
2016-01-26 22:52:56 +0600

    to fix compilation issue in linux

--- utils_test/utils_test/stdafx.cpp
+++ utils_test/utils_test/stdafx.cpp
@@ -9,7 +9,6 @@
 #include <string>
 #include <exception>
 #include <boost/filesystem.hpp>
-#include <Windows.h>
 
 #ifndef _MSC_VER
 #include <boost/test/included/unit_test.hpp>
@@ -18,11 +17,37 @@
 using namespace std;
 using namespace boost::filesystem;
 
-string get_default_test_files_dir_path()
+
+#ifdef _MSC_VER
+
+#include <Windows.h>
+std::string get_exe_full_path()
 {
 	char pBuf[MAX_PATH + 1];
 	int bytes = GetModuleFileNameA(NULL, pBuf, MAX_PATH);
-	path p(pBuf);
+	return pBuf;
+}
+
+#elif
+
+#include <unistd.h>
+std::string get_exe_full_path()
+{
+	char buff[PATH_MAX + 1];
+	ssize_t len = ::readlink("/proc/self/exe", buff, sizeof(buff));
+
+	if(len == -1)
+		throw logic_error("Unable to get executable path.");
+
+	buff[len] = '\0';
+	return buff;
+}
+
+#endif
+
+string get_default_test_files_dir_path()
+{
+	path p(get_exe_full_path());
 	p.remove_filename();
 	p /= "../../test_files/";
 	if (!exists(p))
